60217b66f36fd1f033c9b505db2975dbb860529f
Terri <terriyiu@gmail.com>
2023-10-08 14:40:50 -0400

    created prompt for the number of lines

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-// 1. Deposit some money
+// 1. Deposit some money - DONE
 // 2. Determine number of lines to bet on
 // 3. Collect a bet amount
 // 4. Spin the slot machine
@@ -8,17 +8,37 @@
 
 //parseFloat -> converts string into floating point value, i.e '17.2' -> 17.2
 //isNaN = checks if something is not a number
+// while statement = Loop, (true) = condition, as long as its true -> infinite loop 
 
 
 const prompt = require('prompt-sync')();
 
 const deposit = () => {
-    const depositAmount = prompt('Enter a deposit amount: ');
-    const numberDepositAmount = parseFloat(depositAmount);
+    while (true) {
+        const depositAmount = prompt('Enter a deposit amount: ');
+        const numberDepositAmount = parseFloat(depositAmount);
 
-    if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-        console.log('Invalid deposit amount, try again.')
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log('Invalid deposit amount, try again.')
+        } else {
+            return numberDepositAmount;
+        }
     }
 }
 
-deposit();
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt('Enter the number of lines to bet on (1-3): ');
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log('Invalid number of lines, try again.')
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const depositAmount = deposit();
+// console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
